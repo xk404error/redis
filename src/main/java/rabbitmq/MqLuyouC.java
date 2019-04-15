9fb63e58f1a7eeb49145b51f6b6e76a575491de3
xk404error <xikai404error@outlook.com>
2019-04-15 09:56:33 +0800

    报错修改

--- src/main/java/rabbitmq/MqLuyouC.java
+++ src/main/java/rabbitmq/MqLuyouC.java
@@ -21,6 +21,7 @@ public class MqLuyouC {
         System.out.println(" [*] Waiting for messages. To exit press CTRL+V");
         //定义回调抓取消息
         Consumer consumer = new DefaultConsumer(channel) {
+            @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
                                        byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
